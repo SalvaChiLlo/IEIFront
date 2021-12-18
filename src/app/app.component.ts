e4540dc58658d735fd1e20d811afb33e68f744d7
SalvaChLl <salvachll@gmail.com>
2021-12-18 01:34:54 +0100

    servicio implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { CoreService } from './services/core.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'IEIFront';
+
+  constructor(private core: CoreService) {
+
+  }
+
+  ngOnInit(): void {
+    this.core.getBibliotecaById(1).subscribe(response => {
+      console.log('getBibliotecaById', response);
+    })
+    this.core.getBibliotecas('', '', 'Valencia', 'Privada').subscribe(response => {
+      console.log('getBibliotecas', response);
+    })
+    this.core.getCodPostalesYLocalidades('Valencia').subscribe(response => {
+      console.log('getCodPostalesYLocalidades', response);
+    })
+    this.core.getProvincias().subscribe(response => {
+      console.log('getProvincias', response);
+    })
+    this.core.getTipos().subscribe(response => {
+      console.log('getTipos', response);
+    })
+  }
 }
--- src/app/services/core.service.ts
+++ src/app/services/core.service.ts
@@ -1,9 +1,33 @@
+import { BibliotecaModel, ProvinciumModel } from './../../../../IEIBack/src/models/biblioteca.models';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoreService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBibliotecas(nombreLocalidad: string, codigoPostal: string, nombreProvincia: string, tipo: string): Observable<BibliotecaModel[]> {
+    return this.http.get<BibliotecaModel[]>(environment.baseBackendURL + `/api/bibliotecas?nombreLocalidad=${nombreLocalidad}&codigoPostal=${codigoPostal}&nombreProvincia=${nombreProvincia}&tipo=${tipo}`);
+  }
+
+  getBibliotecaById(id: number): Observable<BibliotecaModel> {
+    return this.http.get<BibliotecaModel>(environment.baseBackendURL + `/api/bibliotecas/${id}`);
+  }
+
+  getCodPostalesYLocalidades(nombreProvincia: string): Observable<any> {
+    return this.http.get(environment.baseBackendURL + `/api/bibliotecas/cp?nombreProvincia=${nombreProvincia}`);
+  }
+
+  getProvincias(): Observable<ProvinciumModel[]> {
+    return this.http.get<ProvinciumModel[]>(environment.baseBackendURL + '/api/provincias');
+  }
+
+  getTipos(): Observable<any> {
+    return this.http.get(environment.baseBackendURL + '/api/bibliotecas/tipos');
+  }
 }
